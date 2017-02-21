e2e44413c42519336ea50f4b00865748683f7d68
azamat96 <azamat.sdu@bk.ru>
2017-02-22 01:20:38 +0600

    first postman request

--- index.js
+++ index.js
@@ -3,26 +3,39 @@ var app = express();
 var bodyParser = require('body-parser');
 
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
-
 app.get('/', function (req, res) {
-    res.sendFile( __dirname + "/index.html");
+    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.post('/addUser', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        email: req.body.email,
+        password: req.body.password
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 });
 
-app.post('/process_get', urlencodedParser,function (req, res) {
-   response = {
-       first_name: req.body.first_name,
-       last_name: req.body.last_name
-   };
-   console.log(req);
-   res.send(JSON.stringify(response));
+app.post('/login', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 });
+
+
 
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
-
     console.log("Example app listening at http://%s:%s", host, port)
 });
